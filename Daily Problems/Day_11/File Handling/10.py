5e26cd6760695b67bdfb104edce0fefe09f164ce
shantanu-Rohile <ca22075shantanurohile@gmail.com>
2026-09-08 10:32:36 +0530

    7/9/2026

--- Daily Problems/Day_11/File Handling/10.py
+++ Daily Problems/Day_11/File Handling/10.py
@@ -1,1 +1,18 @@
-10. Word Frequency Counter
+# 10. Word Frequency Counter
+
+def word_frequency(name):
+    with open(name,"r") as file:
+        data= file.read()
+
+        list1= data.split()
+    frequecy = {}
+
+    for i in list1:
+        if i in frequecy:
+            frequecy.update({i:frequecy.get(i)+1})
+        else:
+            frequecy.update({i:1})
+    return frequecy
+
+print(word_frequency("demo.txt"))
+
